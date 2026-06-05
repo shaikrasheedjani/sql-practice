3f73465b8f24009a839146108ea4eb33a58d3a94
Rasheed Jani SHAIK <shaikrasheedjani@gmail.com>
2026-06-05 13:56:33 +0200

    Create 20_project_employees_i.sql

---
+++ easy/20_project_employees_i.sql
@@ -0,0 +1,9 @@
+-- Problem: Project Employees I
+-- Link: https://leetcode.com/problems/project-employees-i/description/
+
+select p.project_id, round(avg(e.experience_years),2) as average_years 
+  from Project p 
+  left join Employee e on p.employee_id = e.employee_id 
+  group by project_id
+
+-- Concepts: SELECT, LEFT JOIN, GROUP BY operator

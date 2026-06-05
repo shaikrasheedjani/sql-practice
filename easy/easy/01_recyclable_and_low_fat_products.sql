024b783c0d0f35faca93aa9be27aee90d2a02f40
Rasheed Jani SHAIK <shaikrasheedjani@gmail.com>
2026-06-05 11:10:43 +0200

    Delete easy directory

--- easy/easy/01_recyclable_and_low_fat_products.sql
+++
@@ -1,9 +0,0 @@
--- Problem: Recyclable and Low Fat Products
--- Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
-
-SELECT product_id 
-FROM Products
-WHERE low_fats = 'Y' 
-AND recyclable = 'Y'
-
--- Concepts: SELECT, WHERE, AND operator

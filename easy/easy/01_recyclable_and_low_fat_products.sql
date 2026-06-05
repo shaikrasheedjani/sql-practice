b72edcfc9b29d96f5e522d7ecb2ada93d8423122
Rasheed Jani SHAIK <shaikrasheedjani@gmail.com>
2026-06-05 11:08:25 +0200

    Create 01_recyclable_and_low_fat_products.sql

---
+++ easy/easy/01_recyclable_and_low_fat_products.sql
@@ -0,0 +1,9 @@
+-- Problem: Recyclable and Low Fat Products
+-- Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+SELECT product_id 
+FROM Products
+WHERE low_fats = 'Y' 
+AND recyclable = 'Y'
+
+-- Concepts: SELECT, WHERE, AND operator

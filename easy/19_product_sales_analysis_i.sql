3612ace613f70504a99d3ebbad7172a695014a6d
Rasheed Jani SHAIK <shaikrasheedjani@gmail.com>
2026-06-05 13:54:28 +0200

    Update and rename 19_product_sales_analysis.sql to 19_product_sales_analysis_i.sql

--- easy/19_product_sales_analysis_i.sql
+++ easy/19_product_sales_analysis_i.sql
@@ -1,4 +1,4 @@
--- Problem: Product Sales Analysis
+-- Problem: Product Sales Analysis I
 -- Link: https://leetcode.com/problems/product-sales-analysis-i/description/
 
 select p.product_name, s.year, s.price from Sales s left join Product p on s.product_id=p.product_id
